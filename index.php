5d5a2e3ca1bcf62270abccb7e5854a018eba4414
Ulf Wilke <ulf.wilke@intellishop.ag>
2021-09-16 21:07:31 +0200

    php errors

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 
 <?php
-session_start();
+
+
+phpinfo();
+
+
+//session_start();
 
 // Including Database connection
-require_once "pdo.php";
+// require_once "pdo.php";
 
+/*
 if (isset($_SESSION['test'])) {
     echo $_SESSION['test'];
 }
+*/
 
 // Query for displaying data
-$stmt = $pdo->query("SELECT first_name, last_name, headline, user_id, profile_id FROM Profile");
-$elements = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//$stmt = $pdo->query("SELECT first_name, last_name, headline, user_id, profile_id FROM Profile");
+//$elements = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
