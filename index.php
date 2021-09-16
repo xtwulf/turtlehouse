3ae4f02b873980f05b5f507ca74e195422b82562
Ulf Wilke <ulf.wilke@intellishop.ag>
2021-09-16 21:03:10 +0200

    added php to index

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+session_start();
+
+// Including Database connection
+require_once "pdo.php";
+
+if (isset($_SESSION['test'])) {
+    echo $_SESSION['test'];
+}
+
+// Query for displaying data
+$stmt = $pdo->query("SELECT first_name, last_name, headline, user_id, profile_id FROM Profile");
+$elements = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
+<h1>TEST</h1>
