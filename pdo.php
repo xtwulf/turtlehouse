4e988100f00230156c7f16809d57b504398c946a
Ulf Wilke <ulf.wilke@intellishop.ag>
2021-09-17 08:13:58 +0200

    changed db credentials in pdo.php

--- pdo.php
+++ pdo.php
@@ -1,7 +1,8 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc', 
-   'mister_x', 'pw1234');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=testdatenbank', 
+   'testbenutzer', 'raspberry');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
 ?>
