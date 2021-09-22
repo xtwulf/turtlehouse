98783dea77e323e6d6452518ac3eaccc8e3cc324
xtwulf <ulf-wilke@web.de>
2021-09-22 19:46:39 +0200

    added card for lowest temp

--- index.php
+++ index.php
@@ -497,6 +497,29 @@ else {
               </div>
             </div>
 
+            <!-- Card: Min Temperatur -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Niedrigste Temperatur</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">2245</div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <span class="text-info mr-2">12.08.2021</span>
+                        <!-- <span>Since yesterday</span> -->
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-snowflake fa-2x text-info"></i>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
             <!-- Card: Choose control method -->
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card h-100">
