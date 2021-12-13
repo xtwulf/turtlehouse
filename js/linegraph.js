537f8743b03927b55efd63d41e5637fca64ee76f
Ulf Wilke <ulf.wilke@intellishop.ag>
2021-12-13 14:39:51 +0100

    changed path on linegraph.js

--- js/linegraph.js
+++ js/linegraph.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $.ajax({
-      url : "/php/followersdata.php",
+      url : "/php/getdata.php",
       type : "GET",
       success : function(data){
         console.log(data);
